b0380348cd3acc29b46bd337d810532eb6fd54c4
Garrett Sutula <Garrett.Sutula@UniversalOrlando.com>
2018-07-14 23:40:09 -0400

    added warning on classes

--- demos/class.mjs
+++ demos/class.mjs
@@ -2,4 +2,5 @@ import Person from '../class';
 
 const person = new Person('garrett', 'sutula');
 person.first = 'Maverick';
+person.age = 21; // classes are just objects, don't believe the hype
 person.sayName();
